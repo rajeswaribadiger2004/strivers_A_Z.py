87465188c887f2e058832cba97b7312e2041d7dc
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-09-29 23:49:00 +0530

    Create Armstrong.py

---
+++ Armstrong.py
@@ -0,0 +1,14 @@
+#sum of the digit raise to the power of length of no equal to total
+class Solution:
+  def armstrong(self,num:int)->bool:
+    k=len(str(num))
+    n=num
+    total=0
+    while n>0:
+      dig=n%10
+      total += dig ** k     
+      n//=10
+    return total==num
+    
+s = Solution()
+print(s.armstrong(153))
