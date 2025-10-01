93dfc63d07105a0ebc8d3f34f7c539748e287427
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-10-01 19:30:29 +0530

    Update recursive(1_N).py

--- recursive(1_N).py
+++ recursive(1_N).py
@@ -9,3 +9,17 @@ if __name__=="__main__":
     sol=Solution()
     n=4
     sol.func(1,n)
+
+  
+# prg for N-1
+ class Solution:
+  def func(self,i:int,n:int)->None:
+    if i,1:
+      return
+    print(i)
+    
+    self.func(i-1,n)
+if __name__=="__main__":
+    sol=Solution()
+    n=4
+    sol.func(n,n)
