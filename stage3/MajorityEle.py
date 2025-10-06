5d41bdaa8ba23b0e9809e22c4d59310a160cc76e
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-10-06 11:09:47 +0530

    Create MajorityEle.py

---
+++ stage3/MajorityEle.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+  def Majority(self,arr:List[int])->int:
+    n=len(arr)
+    count=Counter(arr)
+    for num,freq in items.count():
+      if freq>n//2:
+        return num
+
+    return -1
+    
